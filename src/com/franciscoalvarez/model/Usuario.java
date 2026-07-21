f9ebd9dd7f6733cffc8eb5d99f67269bf571e3e3
FranciscoAlvarez <falvarez-2023239@kinal.edu.gt>
2026-07-21 11:31:10 -0600

    Usuario

--- src/com/franciscoalvarez/model/Usuario.java
+++ src/com/franciscoalvarez/model/Usuario.java
@@ -9,5 +9,73 @@ package com.franciscoalvarez.model;
  * @author Francisco Franco
  */
 public class Usuario {
-    
+
+    private int idusuario;
+    private String nombrecompleto;
+    private String username;
+    private String email;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idusuario, String nombrecompleto, String username, String email, String password) {
+        this.idusuario = idusuario;
+        this.nombrecompleto = nombrecompleto;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Usuario(String nombrecompleto, String username, String email, String password) {
+        this.nombrecompleto = nombrecompleto;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombrecompleto() {
+        return nombrecompleto;
+    }
+
+    public void setNombrecompleto(String nombrecompleto) {
+        this.nombrecompleto = nombrecompleto;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return nombrecompleto + " (" + username + ")";
+    }
 }
